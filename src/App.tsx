3a32cf818208550b24af85327aa832c4249d2381
GarrixA <aphrogarrix10@gmail.com>
2024-10-31 12:37:36 +0200

    feat(setup): main dependances setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import { Button } from "@/components/ui/button";
+
 const App = () => {
 	return (
 		<>
-			<div>Hello world!</div>
+			<div>
+				Hello world!
+				<Button className="bg-red-500 text-black">Click me!</Button>
+			</div>
 		</>
 	);
 };
